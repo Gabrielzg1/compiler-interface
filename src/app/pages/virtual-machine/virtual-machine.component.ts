edcf53836d7a89b32429f3d8b69fa2178ad143b0
Gabrielzg1 <gabrielzg173@gmail.com>
2024-11-06 23:39:06 -0300

    clean

--- src/app/pages/virtual-machine/virtual-machine.component.ts
+++ src/app/pages/virtual-machine/virtual-machine.component.ts
@@ -1,35 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { PoButtonModule, PoCheckboxModule, PoRadioGroupModule, PoTableModule } from '@po-ui/ng-components';
+import {
+  PoButtonModule,
+  PoCheckboxModule,
+  PoRadioGroupModule,
+  PoTableModule,
+} from '@po-ui/ng-components';
 import axios from 'axios';
-
 
 @Component({
   selector: 'app-code-execution',
   standalone: true,
-  imports: [PoTableModule, PoButtonModule, PoRadioGroupModule, PoCheckboxModule, FormsModule ,CommonModule],
+  imports: [
+    PoTableModule,
+    PoButtonModule,
+    PoRadioGroupModule,
+    PoCheckboxModule,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './virtual-machine.component.html',
-  styleUrls: ['./virtual-machine.component.css']
+  styleUrls: ['./virtual-machine.component.css'],
 })
-export class VirtualMachineComponent  implements OnInit{
+export class VirtualMachineComponent implements OnInit {
   isExecuting = false;
   index: number = 0;
-  s: number = 0; 
-  stepByStep: boolean = false; 
+  s: number = -1;
+  stepByStep: boolean = false;
 
-  instructions: Array<{ line: number, instruction: string, attribute1: string | null, attribute2: string | null }> = [
-    { instruction: "START", attribute1: null, attribute2: null },                       // Início do programa
-  
+  instructions: Array<{
+    line: number;
+    instruction: string;
+    attribute1: string | null;
+    attribute2: string | null;
+  }> = [
+    { instruction: 'START', attribute1: null, attribute2: null }, // Início do programa
+
     // Aloca espaço para variáveis a, b e o resultado
-    { instruction: "ALLOC", attribute1: "0", attribute2: "3" },  // Aloca três posições para a, b e resultado
-    {instruction: "DALLOC", attribute1: "0", attribute2: "3"}
-  
+    { instruction: 'ALLOC', attribute1: '0', attribute2: '4' }, // Aloca três posições para a, b e resultado
+
+    // Outras instruções podem ser adicionadas aqui...
+
+    { instruction: 'DALLOC', attribute1: '0', attribute2: '3' },
+    { instruction: 'HLT', attribute1: null, attribute2: null },
   ].map((instruction, index) => ({ line: index + 1, ...instruction }));
-  
-  
-  
-  stack: Array<{ address: number, value: number }> = [];
+
+  stack: Array<number> = [];
 
   currentLine: number | null = null;
   intervalId: any;
@@ -38,277 +55,215 @@ export class VirtualMachineComponent  implements OnInit{
 
   ngOnInit() {
     this.mapLabels(); // Chama o mapeamento ao inicializar o componente
-    axios.get('http://127.0.0.1:5000/get_obj')
-    .then(response => {
+    axios.get('http://127.0.0.1:5000/get_obj').then((response) => {
       console.log(response.data);
-    })
+    });
   }
-  
-mapLabels() {
+
+  mapLabels() {
     this.instructions.forEach((inst, index) => {
-        if (inst.instruction == "NULL" && inst.attribute1) {
-            this.labelMap[inst.attribute1] = index ;
-        }
+      if (inst.instruction == 'NULL' && inst.attribute1) {
+        this.labelMap[inst.attribute1] = index;
+      }
     });
 
-    console.log("Label Map:", this.labelMap);
-}
+    console.log('Label Map:', this.labelMap);
+  }
 
-  executeInstruction(instruction: string, param1: string | null, param2: string | null) {
+  executeInstruction(
+    instruction: string,
+    param1: string | null,
+    param2: string | null
+  ) {
     switch (instruction) {
-      case 'START': 
-        console.log("Início do programa");
+      case 'START':
+        console.log('Início do programa');
         this.s = -1;
         this.isExecuting = true;
         break;
-  
+
       case 'HLT':
-        console.log("Fim do programa");
+        console.log('Fim do programa');
         this.isExecuting = false;
         clearTimeout(this.intervalId);
         return;
-  
+
       case 'LDC': // Carregar constante
         this.s += 1;
-        this.stack[this.s] = { address: this.s, value: parseInt(param1 || "0", 10) };
+        this.stack[this.s] = parseInt(param1 || '0', 10);
         break;
-  
+
       case 'LDV': // Carregar valor
-        const n = parseInt(param1 || "0", 10);
-        if (this.stack[n] !== undefined) {
-          this.s += 1;
-          this.stack[this.s] = { address: this.s, value: this.stack[n].value };
-        } else {
-          console.warn(`Tentativa de acesso a posição não inicializada na pilha: ${n}`);
-        }
+        const n = parseInt(param1 || '0', 10);
+        this.s += 1;
+        this.stack[this.s] = this.stack[n];
         break;
-  
+
       case 'STR': // Armazenar valor
-        const storeIndex = parseInt(param1 || "0", 10);
-        if (this.stack[this.s] !== undefined) {
-          this.stack[storeIndex] = { address: storeIndex, value: this.stack[this.s].value };
-          this.s -= 1;
-        } else {
-          console.warn(`Tentativa de armazenar valor de posição não inicializada: ${this.s}`);
-        }
+        const storeIndex = parseInt(param1 || '0', 10);
+        this.stack[storeIndex] = this.stack[this.s];
+        this.s -= 1;
         break;
-  
+
       case 'ADD': // Somar
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value += this.stack[this.s].value;
-          this.s -= 1;
-        } else {
-          console.warn("Operação de soma não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] += this.stack[this.s];
+        this.s -= 1;
         break;
-  
+
       case 'SUB': // Subtrair
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value -= this.stack[this.s].value;
-          this.s -= 1;
-        } else {
-          console.warn("Operação de subtração não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] -= this.stack[this.s];
+        this.s -= 1;
         break;
-  
+
       case 'MULT': // Multiplicar
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value *= this.stack[this.s].value;
-          this.s -= 1;
-        } else {
-          console.warn("Operação de multiplicação não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] *= this.stack[this.s];
+        this.s -= 1;
         break;
-  
+
       case 'DIVI': // Dividir
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          if (this.stack[this.s].value !== 0) { // Evita divisão por zero
-            this.stack[this.s - 1].value = Math.floor(this.stack[this.s - 1].value / this.stack[this.s].value);
-            this.s -= 1;
-          } else {
-            console.error("Erro: Divisão por zero.");
-          }
+        if (this.stack[this.s] !== 0) {
+          // Evita divisão por zero
+          this.stack[this.s - 1] = Math.floor(
+            this.stack[this.s - 1] / this.stack[this.s]
+          );
+          this.s -= 1;
         } else {
-          console.warn("Operação de divisão não pôde ser executada por falta de valores na pilha.");
+          console.error('Erro: Divisão por zero.');
         }
         break;
-  
+
       case 'INV': // Inverter sinal
-        if (this.stack[this.s] !== undefined) {
-          this.stack[this.s].value = -this.stack[this.s].value;
-        } else {
-          console.warn("Tentativa de inverter sinal de posição não inicializada na pilha.");
-        }
+        this.stack[this.s] = -this.stack[this.s];
         break;
-  
+
       case 'AND': // Conjunção
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value === 1 && this.stack[this.s].value === 1) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação AND não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] === 1 && this.stack[this.s] === 1 ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'OR': // Disjunção
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value === 1 || this.stack[this.s].value === 1) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação OR não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] === 1 || this.stack[this.s] === 1 ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'NEG': // Negação
-        if (this.stack[this.s] !== undefined) {
-          this.stack[this.s].value = 1 - this.stack[this.s].value;
-        } else {
-          console.warn("Tentativa de realizar NEG em posição não inicializada na pilha.");
-        }
+        this.stack[this.s] = 1 - this.stack[this.s];
         break;
-  
+
       case 'CME': // Comparar menor
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value < this.stack[this.s].value) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação CME não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] < this.stack[this.s] ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'CMA': // Comparar maior
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value > this.stack[this.s].value) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação CMA não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] > this.stack[this.s] ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'CEQ': // Comparar igual
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value === this.stack[this.s].value) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação CEQ não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] === this.stack[this.s] ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'CDIF': // Comparar desigual
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value !== this.stack[this.s].value) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação CDIF não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] !== this.stack[this.s] ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'CMEQ': // Comparar menor ou igual
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value <= this.stack[this.s].value) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação CMEQ não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] <= this.stack[this.s] ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'CMAQ': // Comparar maior ou igual
-        if (this.s > 0 && this.stack[this.s] !== undefined && this.stack[this.s - 1] !== undefined) {
-          this.stack[this.s - 1].value = (this.stack[this.s - 1].value >= this.stack[this.s].value) ? 1 : 0;
-          this.s -= 1;
-        } else {
-          console.warn("Operação CMAQ não pôde ser executada por falta de valores na pilha.");
-        }
+        this.stack[this.s - 1] =
+          this.stack[this.s - 1] >= this.stack[this.s] ? 1 : 0;
+        this.s -= 1;
         break;
-  
+
       case 'ALLOC': // Alocar memória
-        const mAlloc = parseInt(param1 || "0", 10);
-        const nAlloc = parseInt(param2 || "0", 10);
+        const mAlloc = parseInt(param1 || '0', 10);
+        const nAlloc = parseInt(param2 || '0', 10);
         for (let k = 0; k < nAlloc; k++) {
           this.s += 1;
-          // Inicializa o valor na posição da pilha se não existir
-          this.stack[this.s] = { address: this.s, value: this.stack[mAlloc + k]?.value || 0 };
+          this.stack[this.s] = this.stack[mAlloc + k] || 0;
         }
         break;
-  
+
       case 'DALLOC': // Desalocar memória
-        const mDalloc = parseInt(param1 || "0", 10);
-        const nDalloc = parseInt(param2 || "0", 10);
+        const mDalloc = parseInt(param1 || '0', 10);
+        const nDalloc = parseInt(param2 || '0', 10);
         for (let k = nDalloc - 1; k >= 0; k--) {
-          if (this.stack[this.s] !== undefined) {
-            this.stack[mDalloc + k].value = this.stack[this.s].value;
-            this.s -= 1;
-          } else {
-            console.warn(`Tentativa de desalocar posição não inicializada: ${this.s}`);
-          }
+          this.stack[mDalloc + k] = this.stack[this.s];
+          this.s -= 1;
         }
         break;
-  
+
       case 'CALL': // Chamar procedimento ou função
         this.s += 1;
-        this.stack[this.s] = { address: this.s, value: this.index + 1 };
-        const targetCALL = this.labelMap[param1 || ""];
+        this.stack[this.s] = this.index + 1;
+        const targetCALL = this.labelMap[param1 || ''];
         if (targetCALL !== undefined) {
-          this.index = targetCALL;
+          this.index = targetCALL - 1; // Ajusta o índice para a instrução de destino
         } else {
-          console.warn(`Destino da chamada de sub-rotina não encontrado: ${param1}`);
+          console.error(`Rótulo não encontrado: ${param1}`);
         }
         break;
-  
+
       case 'RETURN': // Retornar de procedimento ou função
-        if (this.stack[this.s] !== undefined) {
-          this.index = this.stack[this.s].value;
-          this.s -= 1;
-        } else {
-          console.warn("Tentativa de retornar de uma função sem valor na pilha.");
-        }
+        this.index = this.stack[this.s];
+        this.s -= 1;
         break;
-  
+
       case 'JMP': // Desviar sempre
-        const targetJMP = this.labelMap[param1 || ""];
+        const targetJMP = this.labelMap[param1 || ''];
         if (targetJMP !== undefined) {
-          this.index = targetJMP;
+          this.index = targetJMP - 1; // Ajusta o índice para a instrução de destino
         } else {
-          console.warn(`Destino de JMP não encontrado: ${param1}`);
+          console.error(`Rótulo não encontrado: ${param1}`);
         }
         break;
-  
+
       case 'JMPF': // Desviar se falso
-        const targetJMPF = this.labelMap[param1 || ""];
-        if (targetJMPF !== undefined && this.stack[this.s]?.value === 0) {
-          this.index = targetJMPF;
-        } else {
-          this.index += 1;
+        const targetJMPF = this.labelMap[param1 || ''];
+        if (this.stack[this.s] === 0) {
+          if (targetJMPF !== undefined) {
+            this.index = targetJMPF - 1; // Ajusta o índice para a instrução de destino
+          } else {
+            console.error(`Rótulo não encontrado: ${param1}`);
+          }
         }
         this.s -= 1;
         break;
-  
+
       case 'RD': // Leitura
         this.s += 1;
-        const inputValue = parseInt(prompt("Digite o próximo valor de entrada:") || "0", 10);
-        this.stack[this.s] = { address: this.s, value: inputValue };
+        const inputValue = parseInt(
+          prompt('Digite o próximo valor de entrada:') || '0',
+          10
+        );
+        this.stack[this.s] = inputValue;
         break;
-  
+
       case 'PRN': // Impressão
-        if (this.stack[this.s] !== undefined) {
-          console.log("Imprimindo valor:", this.stack[this.s].value);
-          this.s -= 1;
-        } else {
-          console.warn("Tentativa de imprimir valor de posição não inicializada na pilha.");
-        }
+        console.log('Imprimindo valor:', this.stack[this.s]);
+        this.s -= 1;
         break;
-  
+
       case 'NULL': // Nada
         break;
-  
+
       default:
         console.log(`Instrução desconhecida: ${instruction}`);
     }
   }
-  
-  
-
-
 
   // Método de execução usando `setTimeout` para evitar congestionamento com `setInterval`
   startExecution() {
@@ -321,17 +276,17 @@ mapLabels() {
     // Verifica se o índice atual está dentro do limite do array de instruções
     if (this.index < this.instructions.length) {
       const currentInstruction = this.instructions[this.index];
-      
+
       if (currentInstruction) {
-        const { instruction, attribute1, attribute2, line } = currentInstruction;
-        this.executeInstruction(instruction, attribute1, attribute2);
+        const { instruction, attribute1, attribute2, line } =
+          currentInstruction;
         this.currentLine = line;
+        this.executeInstruction(instruction, attribute1, attribute2);
       }
-  
+
       // Avança o índice para a próxima instrução
       this.index++;
-      console.log(this.s)
-  
+
       // Define o tempo de execução com base no modo step-by-step
       const delay = this.stepByStep ? 1000 : 1;
       this.intervalId = setTimeout(() => this.executeNextInstruction(), delay);
@@ -339,7 +294,6 @@ mapLabels() {
       this.finishExecution(); // Finaliza a execução caso todas instruções tenham sido processadas
     }
   }
-  
 
   stopExecution() {
     this.isExecuting = false;
@@ -351,7 +305,7 @@ mapLabels() {
     clearTimeout(this.intervalId);
     this.index = 0;
     this.s = -1;
-    this.stack = []; 
+    this.stack = [];
     this.currentLine = null;
   }
 
